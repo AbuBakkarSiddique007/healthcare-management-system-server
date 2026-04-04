7302100bb4a4a5a4454e8826538d7822c99c84bd
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2026-04-05 02:47:02 +0600

    feat(admin): scaffold admin module (controller, service, routes, validation)

---
+++ src/app/module/admin/admin.controller.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../shared/catchAsync";
+import { AdminService } from "./admin.service";
+import { sendResponse } from "../../shared/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+
+const getAllAdmins = catchAsync(
+    async (req: Request, res: Response) => {
+        const result = await AdminService.getAllAdmins();
+
+        sendResponse(res, {
+            httpStatusCode: StatusCodes.OK,
+            success: true,
+            message: "Admins fetched successfully",
+            data: result,
+        })
+    }
+)
+
+
+const getAdminById = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const admin = await AdminService.getAdminById(id as string);
+
+        sendResponse(res, {
+            httpStatusCode: StatusCodes.OK,
+            success: true,
+            message: "Admin fetched successfully",
+            data: admin,
+        })
+    }
+)
+
+const updateAdmin = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const payload = req.body;
+
+        const updatedAdmin = await AdminService.updateAdmin(id as string, payload);
+
+        sendResponse(res, {
+            httpStatusCode: StatusCodes.OK,
+            success: true,
+            message: "Admin updated successfully",
+            data: updatedAdmin,
+        })
+    }
+)
+
+const deleteAdmin = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const user = req.user;
+
+        const result = await AdminService.deleteAdmin(id as string, user);
+
+        sendResponse(res, {
+            httpStatusCode: StatusCodes.OK,
+            success: true,
+            message: "Admin deleted successfully",
+            data: result,
+        })
+    }
+
+)
+
+export const AdminController = {
+    getAllAdmins,
+    updateAdmin,
+    deleteAdmin,
+    getAdminById,
+};
